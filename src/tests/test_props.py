f2136c9467a9b883dd7f383317660326e5c9e316
Eric Gazoni <eric.gazoni@gmail.com>
2010-04-09 23:42:23 +0200

    added read methods for document properties

---
+++ src/tests/test_props.py
@@ -0,0 +1,48 @@
+'''
+Copyright (c) 2010 openpyxl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+@license: http://www.opensource.org/licenses/mit-license.php
+@author: Eric Gazoni
+'''
+
+import os.path as osp
+from tests.helper import BaseTestCase, DATADIR, TMPDIR
+
+from openpyxl.reader.workbook import fill_properties
+from openpyxl.shared.zip import ZipArchive
+from openpyxl.workbook import DocumentProperties, DocumentSecurity, Workbook
+
+class TestReader(BaseTestCase):
+
+    def setUp(self):
+
+        self.gen_filename = osp.join(DATADIR, 'genuine', 'empty.xlsx')
+
+    def test_read_properties(self):
+
+        prop = DocumentProperties()
+
+        zip = ZipArchive(filename = self.gen_filename)
+
+        fill_properties(properties = prop, archive = zip)
+
+        self.assertEqual(prop.creator, '*.*')
+        self.assertEqual(prop.last_modified_by, '*.*')
