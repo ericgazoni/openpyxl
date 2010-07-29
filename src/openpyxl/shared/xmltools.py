8bfbe6ebe55fdfd22d722b86c35be19bf7b0ca00
Eric Gazoni <none@none>
2010-07-29 09:55:48 +0200

    [xml] added automatic indent of produced files, as now comparison function can work off Excel native (non-)indented files, and human readable files

--- src/openpyxl/shared/xmltools.py
+++ src/openpyxl/shared/xmltools.py
@@ -30,6 +30,23 @@ def get_document_content(xml_node):
 
     fl = StringIO()
 
+    pretty_indent(xml_node)
+
     ElementTree(xml_node).write(file = fl, encoding = 'UTF-8')
 
     return fl.getvalue()
+
+def pretty_indent(elem, level = 0):
+    i = "\n" + level * "  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            pretty_indent(elem, level + 1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
