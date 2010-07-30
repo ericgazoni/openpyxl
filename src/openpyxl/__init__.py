689ebaf442d964cc02fa452ba5fdf13abce49c75
Eric Gazoni <eric.gazoni@gmail.com>
2010-07-30 20:43:05 +0200

    [version] bumping version number

--- src/openpyxl/__init__.py
+++ src/openpyxl/__init__.py
@@ -24,8 +24,8 @@ THE SOFTWARE.
 '''
 
 __major__ = 1       # for major interface/format changes
-__minor__ = 0       # for minor interface/format changes
-__release__ = 4     # for tweaks, bug-fixes, or development
+__minor__ = 1       # for minor interface/format changes
+__release__ = 0     # for tweaks, bug-fixes, or development
 
 __version__ = '%d.%d.%d' % (__major__,
                             __minor__,
