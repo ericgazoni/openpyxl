af6697afb591d5906a82974d0b65bab5c831ef27
Eric Gazoni <eric.gazoni@gmail.com>
2010-09-11 15:07:12 +0200

    [doc] updated docstring

--- src/openpyxl/worksheet.py
+++ src/openpyxl/worksheet.py
@@ -250,6 +250,8 @@ class Worksheet(object):
         
         :raise: InsufficientCoordinatesException when coordinate or (row and column) are not given
         
+        :rtype: :class:`openpyxl.cell.Cell` 
+        
         """
 
         if not coordinate:
