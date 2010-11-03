589d5c9e083b2e2b13431d7a1896035429a40a1f
Eric Gazoni <eric.gazoni@gmail.com>
2010-11-03 13:41:02 +0100

    [reader] replaced sax parser by xml tree iterator

--- openpyxl/reader/worksheet.py
+++ openpyxl/reader/worksheet.py
@@ -26,55 +26,50 @@
 """Reader for a single worksheet."""
 
 # Python stdlib imports
-from xml.sax import parseString
-from xml.sax.handler import ContentHandler
+try:
+    from xml.etree.cElementTree import iterparse
+except ImportError:
+    from xml.etree.ElementTree import iterparse
+from itertools import ifilter
+from StringIO import StringIO
 
 # package imports
 from openpyxl.cell import Cell
 from openpyxl.worksheet import Worksheet
 
+def filter_cells((event, element)):
 
-class WorksheetReader(ContentHandler):
-    """An xml.sax handler for reading xlsx worksheets."""
+    return element.tag == '{http://schemas.openxmlformats.org/spreadsheetml/2006/main}c'
 
-    def __init__(self, ws, string_table, style_table):
-        ContentHandler.__init__(self)
-        self.ws = ws
-        self.string_table = string_table
-        self.style_table = style_table
-        self.read_value = False
-        self.data_type = None
-        self.style_id = None
-        self.coordinate = None
+def fast_parse(ws, xml_source, string_table, style_table):
 
-    def startElement(self, name, attrs):
-        """Start reading information for a cell defined in xml."""
-        if name == 'c':
-            self.coordinate = attrs.get('r')
-            self.data_type = attrs.get('t', 'n')
-            self.style_id = attrs.get('s')
-            self.read_value = True
+    source = StringIO(xml_source)
 
-    def characters(self, value):
-        """Unpack the string and style tables."""
-        if self.read_value and value is not None:
-            if self.data_type == Cell.TYPE_STRING:
-                value = self.string_table.get(int(value))
-            self.ws.cell(self.coordinate).value = value
-            if self.style_id is not None:
-                self.ws._styles[self.coordinate] = \
-                        self.style_table.get(int(self.style_id))
+    it = iterparse(source)
 
-    def endElement(self, name):
-        """Stop reading information for a cell defined in xml."""
-        if name == 'c':
-            self.read_value = False
+    for event, element in ifilter(filter_cells, it):
 
+        coordinate = element.get('r')
+        data_type = element.get('t', 'n')
+        style_id = element.get('s')
+        value = element.findtext('{http://schemas.openxmlformats.org/spreadsheetml/2006/main}v')
+
+        if value is not None:
+
+            if data_type == Cell.TYPE_STRING:
+                value = string_table.get(int(value))
+
+            ws.cell(coordinate).value = value
+
+            if style_id is not None:
+                ws._styles[coordinate] = style_table.get(int(style_id))
+
+        # to avoid memory exhaustion, clear the item after use
+        element.clear()
 
 def read_worksheet(xml_source, parent, preset_title,
         string_table, style_table):
     """Read an xml worksheet"""
     ws = Worksheet(parent, preset_title)
-    handler = WorksheetReader(ws, string_table, style_table)
-    parseString(xml_source, handler)
+    fast_parse(ws, xml_source, string_table, style_table)
     return ws
