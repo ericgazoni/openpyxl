56e417c7c4890dfc5e2f59ce38b41656ef481d7d
Eric Gazoni <eric.gazoni@gmail.com>
2011-01-26 10:06:53 +0100

    [version] bump

--- openpyxl/__init__.py
+++ openpyxl/__init__.py
@@ -39,7 +39,7 @@ from openpyxl import writer
 
 __major__ = 1  # for major interface/format changes
 __minor__ = 3  # for minor interface/format changes
-__release__ = 1  # for tweaks, bug-fixes, or development
+__release__ = 2  # for tweaks, bug-fixes, or development
 
 __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
