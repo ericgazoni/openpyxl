5ce9b3070ace184aeafb803876c40181ca2768b2
Eric Gazoni <eric.gazoni@adimian.com>
2011-04-07 11:06:54 +0200

    [version]

--- openpyxl/__init__.py
+++ openpyxl/__init__.py
@@ -39,7 +39,7 @@ from openpyxl import writer
 
 __major__ = 1  # for major interface/format changes
 __minor__ = 5  # for minor interface/format changes
-__release__ = 0  # for tweaks, bug-fixes, or development
+__release__ = 1  # for tweaks, bug-fixes, or development
 
 __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
