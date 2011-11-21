a61eeb6fa19dc015ead46c9275dd1003ff39fcd2
aceMueller <none@none>
2011-11-21 13:29:55 -0600

    enables the 'underline' attribute of the font (the attribute was already in the Font class, but the writer did not output the u token); includes test case

--- openpyxl/writer/styles.py
+++ openpyxl/writer/styles.py
@@ -98,6 +98,7 @@ class StyleWriter(object):
                     SubElement(font_node, 'b')
                 if st.font.italic:
                     SubElement(font_node, 'i')
+                SubElement(font_node, 'u', {'val':st.font.underline})
                 index += 1
 
         fonts.attrib["count"] = str(index)
