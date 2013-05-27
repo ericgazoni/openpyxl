e75bb1c1411761a978467eb69922bbf5a47a5c0e
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-05-27 20:17:13 +0200

    Tests for type inference although this will not work.
    --HG--
    branch : Charts with datetime axes

--- openpyxl/tests/test_chart.py
+++ openpyxl/tests/test_chart.py
@@ -125,8 +125,10 @@ class TestReference(object):
         eq_(self.range.pos2, (9, 0))
 
     def test_get_type(self):
-        self.cell.data_type = 'n'
-        eq_(self.cell.get_type(), 'n')
+        self.cell.values
+        eq_(self.cell.data_type, 'n')
+        self.range.values
+        eq_(self.range.data_type, 'n')
 
     def test_caching_cell(self):
         eq_(self.cell._get_cache(), [0])
