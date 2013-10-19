409ecb44aaf574633cd1289d3463e7f3bfe69ac7
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-10-19 11:56:21 +0200

    Converted some more tests to use tree comparison and renamed them to make a more explicit reference to the return value as the diff.
    --HG--
    branch : feature/Schema_Validation

--- openpyxl/tests/test_chart.py
+++ openpyxl/tests/test_chart.py
@@ -379,19 +379,25 @@ class TestChartWriter(object):
     def test_write_title(self):
         self.cw._write_title(self.root)
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:title><c:tx><c:rich><a:bodyPr /><a:lstStyle /><a:p><a:pPr><a:defRPr /></a:pPr><a:r><a:rPr lang="en-GB" /><a:t>TITLE</a:t></a:r></a:p></c:rich></c:tx><c:layout /></c:title></test>"""
-        assert_equals_string(get_xml(self.root), expected)
+        xml = get_xml(self.root)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
     def test_write_xaxis(self):
 
         self.cw._write_axis(self.root, self.chart.x_axis, '{http://schemas.openxmlformats.org/drawingml/2006/chart}catAx')
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:catAx><c:axId val="60871424" /><c:scaling><c:orientation val="minMax" /></c:scaling><c:axPos val="b" /><c:tickLblPos val="nextTo" /><c:crossAx val="60873344" /><c:crosses val="autoZero" /><c:auto val="1" /><c:lblAlgn val="ctr" /><c:lblOffset val="100" /></c:catAx></test>"""
-        assert_equals_string(get_xml(self.root), expected)
+        xml = get_xml(self.root)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
     def test_write_yaxis(self):
 
         self.cw._write_axis(self.root, self.chart.y_axis, '{http://schemas.openxmlformats.org/drawingml/2006/chart}valAx')
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:valAx><c:axId val="60873344" /><c:scaling><c:orientation val="minMax" /><c:max val="10.0" /><c:min val="0.0" /></c:scaling><c:axPos val="l" /><c:majorGridlines /><c:numFmt formatCode="General" sourceLinked="1" /><c:tickLblPos val="nextTo" /><c:crossAx val="60871424" /><c:crosses val="autoZero" /><c:crossBetween val="between" /><c:majorUnit val="2.0" /></c:valAx></test>"""
-        assert_equals_string(get_xml(self.root), expected)
+        xml = get_xml(self.root)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
     def test_write_series(self):
 
@@ -485,9 +491,8 @@ class TestChartWriter(object):
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:numRef><c:f>'data'!$A$1:$J$1</c:f><c:numCache><c:formatCode>d-mmm</c:formatCode><c:ptCount val="10" /><c:pt idx="0"><c:v>0</c:v></c:pt><c:pt idx="1"><c:v>1</c:v></c:pt><c:pt idx="2"><c:v>2</c:v></c:pt><c:pt idx="3"><c:v>3</c:v></c:pt><c:pt idx="4"><c:v>4</c:v></c:pt><c:pt idx="5"><c:v>5</c:v></c:pt><c:pt idx="6"><c:v>6</c:v></c:pt><c:pt idx="7"><c:v>7</c:v></c:pt><c:pt idx="8"><c:v>8</c:v></c:pt><c:pt idx="9"><c:v>9</c:v></c:pt></c:numCache></c:numRef></test>"""
 
         xml = get_xml(root)
-        compared = compare_xml(xml, expected)
-        eq_(compared, None, compared)
-        eq_(xml, expected)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
 
 class TestScatterChartWriter(object):
@@ -512,8 +517,8 @@ class TestScatterChartWriter(object):
         self.cw._write_axis(self.root, self.scatterchart.x_axis, '{http://schemas.openxmlformats.org/drawingml/2006/chart}valAx')
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:valAx><c:axId val="60871424" /><c:scaling><c:orientation val="minMax" /><c:max val="10.0" /><c:min val="0.0" /></c:scaling><c:axPos val="b" /><c:majorGridlines /><c:numFmt formatCode="General" sourceLinked="1" /><c:title><c:tx><c:rich><a:bodyPr /><a:lstStyle /><a:p><a:pPr><a:defRPr /></a:pPr><a:r><a:rPr lang="en-GB" /><a:t>test x axis title</a:t></a:r></a:p></c:rich></c:tx><c:layout /></c:title><c:tickLblPos val="nextTo" /><c:crossAx val="60873344" /><c:crosses val="autoZero" /><c:auto val="1" /><c:lblAlgn val="ctr" /><c:lblOffset val="100" /><c:crossBetween val="midCat" /><c:majorUnit val="2.0" /></c:valAx></test>"""
         xml = get_xml(self.root)
-        compared = compare_xml(xml, expected)
-        eq_(compared, None, compared)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
     def test_write_yaxis(self):
 
@@ -521,8 +526,8 @@ class TestScatterChartWriter(object):
         self.cw._write_axis(self.root, self.scatterchart.y_axis, '{http://schemas.openxmlformats.org/drawingml/2006/chart}valAx')
         expected = """<?xml version='1.0' encoding='UTF-8'?><test xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:c="http://schemas.openxmlformats.org/drawingml/2006/chart"><c:valAx><c:axId val="60873344" /><c:scaling><c:orientation val="minMax" /><c:max val="10.0" /><c:min val="0.0" /></c:scaling><c:axPos val="l" /><c:majorGridlines /><c:numFmt formatCode="General" sourceLinked="1" /><c:title><c:tx><c:rich><a:bodyPr /><a:lstStyle /><a:p><a:pPr><a:defRPr /></a:pPr><a:r><a:rPr lang="en-GB" /><a:t>test y axis title</a:t></a:r></a:p></c:rich></c:tx><c:layout /></c:title><c:tickLblPos val="nextTo" /><c:crossAx val="60871424" /><c:crosses val="autoZero" /><c:crossBetween val="midCat" /><c:majorUnit val="2.0" /></c:valAx></test>"""
         xml = get_xml(self.root)
-        compared = compare_xml(xml, expected)
-        eq_(compared, None, compared)
+        diff = compare_xml(xml, expected)
+        eq_(diff, None, diff)
 
     def test_write_series(self):
 
