2ba196fa08be8c1bb832699bbb7cd06ceb33b87f
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-10-22 19:49:36 +0200

    Removed because no longer used in __init__
    --HG--
    branch : feature/Pie_Chart

--- openpyxl/chart.py
+++ openpyxl/chart.py
@@ -508,17 +508,11 @@ class GraphChart(Chart):
 
 class BarChart(GraphChart):
 
-    #x_axis = CategoryAxis()
-    #y_axis = ValueAxis()
-
     TYPE = "barChart"
     GROUPING = "clustered"
 
 
 class LineChart(GraphChart):
-
-    #x_axis = CategoryAxis()
-    #y_axis = ValueAxis()
 
     TYPE = "lineChart"
 
