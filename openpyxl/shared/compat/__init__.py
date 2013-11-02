674f4392c5e611e8f8bde8eb4639729f8a97bd75
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-11-02 16:32:55 +0100

    Removed legacy Python < 2.6 compatibility.
    --HG--
    branch : feature/Schema_Validation

--- openpyxl/shared/compat/__init__.py
+++ openpyxl/shared/compat/__init__.py
@@ -23,7 +23,6 @@
 from tempfile import NamedTemporaryFile
 
 from openpyxl.shared.compat.elementtree import iterparse
-from openpyxl.shared.compat.allany import all, any
 from openpyxl.shared.compat.strings import basestring, unicode, StringIO, file, BytesIO
 from openpyxl.shared.compat.numbers import long
 from openpyxl.shared.compat.itertools import xrange, ifilter, iteritems
