1db74f361a1621cf081c60f04a19a7559386f2bd
Charlie Clark <none@none>
2013-11-21 14:59:41 +0100

    Fix for Python 3: __hash__ must be explicited implemented in a class which implements __eq__ or __ne__
    --HG--
    branch : 1.8

--- openpyxl/styles/numbers.py
+++ openpyxl/styles/numbers.py
@@ -118,6 +118,9 @@ class NumberFormat(HashableObject):
             return self.format_code == other.format_code
         return self.format_code == other
 
+    def __hash__(self):
+        return super(NumberFormat, self).__hash__()
+
     def __init__(self):
         self._format_code = self.FORMAT_GENERAL
         self._format_index = 0
