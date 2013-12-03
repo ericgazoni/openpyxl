48fdb10ed8b0f003d32e51028699e505fde62b5d
Charlie Clark <none@none>
2013-12-03 16:13:33 +0100

    Some additional basic fixtures
    --HG--
    branch : 1.8

--- openpyxl/tests/conftest.py
+++ openpyxl/tests/conftest.py
@@ -2,7 +2,36 @@
 
 import pytest
 
+# objects under test
+
 @pytest.fixture
 def NumberFormat():
     from openpyxl.styles import NumberFormat
     return NumberFormat
+
+
+@pytest.fixture
+def Workbook():
+    from openpyxl import Workbook
+    return Workbook
+
+
+@pytest.fixture
+def ws(Workbook):
+    wb = Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = 'data'
+    return ws
+
+
+@pytest.fixture
+def ten_row_sheet(ws):
+    for i in range(10):
+        ws.cell(row=i, column=0).value = i
+    return ws
+
+
+@pytest.fixture
+def ten_column_sheet(ws):
+    ws.append(list(range(10)))
+    return ws
