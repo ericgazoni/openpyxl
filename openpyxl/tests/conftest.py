79ca4b96dd016160e030caefc52b31cd40286f9a
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-12-11 20:23:52 +0100

    Added docstrings to global fixtures.
    --HG--
    branch : 1.8

--- openpyxl/tests/conftest.py
+++ openpyxl/tests/conftest.py
@@ -6,12 +6,14 @@ import pytest
 
 @pytest.fixture
 def NumberFormat():
+    """NumberFormat Class"""
     from openpyxl.styles import NumberFormat
     return NumberFormat
 
 
 @pytest.fixture
 def Workbook():
+    """Workbook Class"""
     from openpyxl import Workbook
     return Workbook
 
@@ -19,66 +21,77 @@ def Workbook():
 
 @pytest.fixture
 def Chart():
+    """Chart class"""
     from openpyxl.chart import Chart
     return Chart
 
 
 @pytest.fixture
 def GraphChart():
+    """GraphicChart class"""
     from openpyxl.chart import GraphChart
     return GraphChart
 
 
 @pytest.fixture
 def Axis():
+    """Axis class"""
     from openpyxl.chart import Axis
     return Axis
 
 
 @pytest.fixture
 def PieChart():
+    """PieChart class"""
     from openpyxl.chart import PieChart
     return PieChart
 
 
 @pytest.fixture
 def LineChart():
+    """LineChart class"""
     from openpyxl.chart import LineChart
     return LineChart
 
 
 @pytest.fixture
 def BarChart():
+    """BarChart class"""
     from openpyxl.chart import BarChart
     return BarChart
 
 
 @pytest.fixture
 def ScatterChart():
+    """ScatterChart class"""
     from openpyxl.chart import ScatterChart
     return ScatterChart
 
 
 @pytest.fixture
 def Reference():
+    """Reference class"""
     from openpyxl.chart import Reference
     return Reference
 
 
 @pytest.fixture
 def Serie():
+    """Serie class"""
     from openpyxl.chart import Serie
     return Serie
 
 
 @pytest.fixture
 def ErrorBar():
+    """ErrorBar class"""
     from openpyxl.chart import ErrorBar
     return ErrorBar
 
 
 @pytest.fixture
 def Image():
+    """Image class"""
     from openpyxl.drawing import Image
     return Image
 
@@ -87,6 +100,7 @@ def Image():
 
 @pytest.fixture
 def ws(Workbook):
+    """Empty worksheet titled 'data'"""
     wb = Workbook()
     ws = wb.get_active_sheet()
     ws.title = 'data'
@@ -95,6 +109,7 @@ def ws(Workbook):
 
 @pytest.fixture
 def ten_row_sheet(ws):
+    """Worksheet with values 0-9 in the first column"""
     for i in range(10):
         ws.cell(row=i, column=0).value = i
     return ws
@@ -102,6 +117,7 @@ def ten_row_sheet(ws):
 
 @pytest.fixture
 def ten_column_sheet(ws):
+    """Worksheet with values 0-9 in the first row"""
     ws.append(list(range(10)))
     return ws
 
@@ -109,6 +125,7 @@ from openpyxl.shared.xmltools import Element
 
 @pytest.fixture
 def root_xml():
+    """Root XML element <test>"""
     return Element("test")
 
 
