3bde8e2b9b0b734e0e9c29b10fe6154ec4f628ee
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-12-16 22:08:59 +0100

    Add Worksheet Class fixture.
    --HG--
    branch : 1.8

--- openpyxl/tests/conftest.py
+++ openpyxl/tests/conftest.py
@@ -17,6 +17,14 @@ def Workbook():
     from openpyxl import Workbook
     return Workbook
 
+
+@pytest.fixture
+def Worksheet():
+    """Worksheet Class"""
+    from openpyxl.worksheet import Worksheet
+    return Worksheet
+
+
 # Charts
 
 @pytest.fixture
