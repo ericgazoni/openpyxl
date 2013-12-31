81e3d947895a0f28fa60167809c108e16ccb3d03
Charlie Clark <charlie.clark@clark-consulting.eu>
2013-12-31 14:49:24 +0100

    Resolves #187 and replaces assertion check with ValueError. No tests fail… which probably mean there are no tests!
    --HG--
    branch : 1.8

--- openpyxl/worksheet.py
+++ openpyxl/worksheet.py
@@ -757,9 +757,10 @@ class Worksheet(object):
     def set_printer_settings(self, paper_size, orientation):
         """Set printer settings """
 
-        self.paper_size = paper_size
-        assert orientation in (self.ORIENTATION_PORTRAIT, self.ORIENTATION_LANDSCAPE), "Values should be %s or %s" % (self.ORIENTATION_PORTRAIT, self.ORIENTATION_LANDSCAPE)
-        self.orientation = orientation
+        self.page_setup.paperSize = paper_size
+        if orientation not in (self.ORIENTATION_PORTRAIT, self.ORIENTATION_LANDSCAPE),:
+            raise ValueError("Values should be %s or %s" % (self.ORIENTATION_PORTRAIT, self.ORIENTATION_LANDSCAPE))
+        self.page_setup.orientation = orientation
 
     def create_relationship(self, rel_type):
         """Add a relationship for this sheet."""
