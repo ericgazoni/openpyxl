81c0a2e684e746c485711b08b63cbb79b97c5834
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-01-14 13:27:55 +0100

    Resolves #246
    --HG--
    branch : 1.8

--- openpyxl/__init__.py
+++ openpyxl/__init__.py
@@ -48,7 +48,7 @@ from openpyxl.reader.excel import load_workbook
 
 __major__ = 1  # for major interface/format changes
 __minor__ = 8  # for minor interface/format changes
-__release__ = 0  # for tweaks, bug-fixes, or development
+__release__ = 1  # for tweaks, bug-fixes, or development
 
 __version__ = '%d.%d.%d' % (__major__, __minor__, __release__)
 
