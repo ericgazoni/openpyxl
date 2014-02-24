6b9ff851a419c79cd3ca3755466ac53674dc3daf
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-02-24 20:44:57 +0100

    Ensure registration of specific namespaces. Test is a bit rough and ready.
    --HG--
    branch : 1.8

--- openpyxl/tests/test_backend.py
+++ openpyxl/tests/test_backend.py
@@ -36,6 +36,7 @@ except ImportError:
 
 from xml.etree.ElementTree import Element as pyElement
 
+
 def test_backend():
     from openpyxl.shared.xmltools import Element
     if LXML is True:
@@ -44,3 +45,12 @@ def test_backend():
         assert Element == cElement
     else:
         assert Element == pyElement
+
+
+def test_namespace_register():
+    from openpyxl.shared.xmltools import Element, tostring
+    from openpyxl.shared.ooxml import SHEET_MAIN_NS
+
+    e = Element('{%s}sheet' % SHEET_MAIN_NS)
+    assert tostring(e).startswith("<s:sheet")
+    #s:sheet xmlns:s="http://schemas.openxmlformats.org/spreadsheetml/2006/main"/>"""
