d61da3a9cc08092a6fe3b1d8f33235c402263210
Charlie Clark <charlie.clark@clark-consulting.eu>
2014-02-26 10:24:08 +0100

    Refactor methods to allow parse_dxfs to use parsing methods (font, fill, border) without putting intelligence into the parsing methods.
    --HG--
    branch : 1.9

--- openpyxl/reader/style.py
+++ openpyxl/reader/style.py
@@ -44,9 +44,9 @@ class SharedStylesParser(object):
         self.parse_custom_num_formats()
         self.parse_color_index()
         self.style_prop['color_index'] = self.color_index
-        self.font_list = self.parse_fonts()
-        self.fill_list = self.parse_fills()
-        self.border_list = self.parse_borders()
+        self.font_list = list(self.parse_fonts())
+        self.fill_list = list(self.parse_fills())
+        self.border_list = list(self.parse_borders())
         self.parse_dxfs()
         self.parse_cell_xfs()
 
@@ -81,56 +81,17 @@ class SharedStylesParser(object):
             nodes = dxfs.findall('{%s}dxf' % SHEET_MAIN_NS)
             for dxf in nodes:
                 dxf_item = {}
-                font_list = self.parse_fonts(dxf)
-                if font_list:
-                    dxf_item['font'] = font_list[0]
-                fill_list = self.parse_fills(dxf)
-                if fill_list:
-                    dxf_item['fill'] = fill_list[0]
-                border_list = self.parse_borders(dxf)
-                if border_list:
-                    dxf_item['border'] = border_list[0]
+                font_node = dxf.find('{%s}font' % SHEET_MAIN_NS)
+                if font_node is not None:
+                    dxf_item['font'] = self.parse_font(font_node)
+                fill_node = dxf.find('{%s}fill' % SHEET_MAIN_NS)
+                if fill_node is not None:
+                    dxf_item['fill'] =  self.parse_fill(fill_node)
+                border_node = dxf.find('{%s}border' % SHEET_MAIN_NS)
+                if border_node is not None:
+                    dxf_item['border'] = self.parse_border(border_node)
                 dxf_list.append(dxf_item)
         self.style_prop['dxf_list'] = dxf_list
-
-
-    def parse_fonts(self, node=False):
-        """Read in the fonts"""
-        font_list = []
-        if node:
-            fonts = node
-        else:
-            fonts = self.root.find('{%s}fonts' % SHEET_MAIN_NS)
-        if fonts is not None:
-            font_nodes = safe_iterator(fonts, '{%s}font' % SHEET_MAIN_NS)
-            for font_node in font_nodes:
-                font = Font()
-                if not node:
-                    fontSizeEl = font_node.find('{%s}sz' % SHEET_MAIN_NS)
-                    if fontSizeEl is not None:
-                        font.size = fontSizeEl.get('val')
-                    fontNameEl = font_node.find('{%s}name' % SHEET_MAIN_NS)
-                    if fontNameEl is not None:
-                        font.name = fontNameEl.get('val')
-                bold = font_node.find('{%s}b' % SHEET_MAIN_NS)
-                if bold is not None:
-                    font.bold = bool(bold.get('val', True))
-                italic = font_node.find('{%s}i' % SHEET_MAIN_NS)
-                if italic is not None:
-                    font.italic = bool(italic.get('val', True))
-                underline =font_node.find('{%s}u' % SHEET_MAIN_NS)
-                if underline is not None:
-                    font.underline = underline.get('val', 'single')
-                strikethrough = font_node.find('{%s}strike' % SHEET_MAIN_NS)
-                if strikethrough is not None:
-                    font.strikethrough = True
-                color = font_node.find('{%s}color' % SHEET_MAIN_NS)
-                if color is not None:
-                    font.color.index = self._get_relevant_color(color)
-                elif node:
-                    font.color = None
-                font_list.append(font)
-        return font_list
 
 
     def _get_relevant_color(self, color):
@@ -151,75 +112,95 @@ class SharedStylesParser(object):
         return value
 
 
-    def parse_fills(self, node=False):
+    def parse_fonts(self):
+        """Read in the fonts"""
+        fonts = self.root.find('{%s}fonts' % SHEET_MAIN_NS)
+        if fonts is not None:
+            for node in safe_iterator(fonts, '{%s}font' % SHEET_MAIN_NS):
+                yield self.parse_font(node)
+
+
+    def parse_font(self, font_node):
+        """Read individual font"""
+        font = Font()
+        fontSizeEl = font_node.find('{%s}sz' % SHEET_MAIN_NS)
+        if fontSizeEl is not None:
+            font.size = fontSizeEl.get('val')
+        fontNameEl = font_node.find('{%s}name' % SHEET_MAIN_NS)
+        if fontNameEl is not None:
+            font.name = fontNameEl.get('val')
+        bold = font_node.find('{%s}b' % SHEET_MAIN_NS)
+        if bold is not None:
+            font.bold = bool(bold.get('val', True))
+        italic = font_node.find('{%s}i' % SHEET_MAIN_NS)
+        if italic is not None:
+            font.italic = bool(italic.get('val', True))
+        underline = font_node.find('{%s}u' % SHEET_MAIN_NS)
+        if underline is not None:
+            font.underline = underline.get('val', 'single')
+        strikethrough = font_node.find('{%s}strike' % SHEET_MAIN_NS)
+        if strikethrough is not None:
+            font.strikethrough = True
+        color = font_node.find('{%s}color' % SHEET_MAIN_NS)
+        if color is not None:
+            font.color.index = self._get_relevant_color(color)
+        return font
+
+
+    def parse_fills(self):
         """Read in the list of fills"""
-        fill_list = []
-        if node:
-            fills = node
-        else:
-            fills = self.root.find('{%s}fills' % SHEET_MAIN_NS)
+        fills = self.root.find('{%s}fills' % SHEET_MAIN_NS)
         if fills is not None:
-            fillNodes = safe_iterator(fills, '{%s}fill' % SHEET_MAIN_NS)
-            for fill in fillNodes:
-                # Rotation is unset
-                patternFill = fill.find('{%s}patternFill' % SHEET_MAIN_NS)
-                if patternFill is not None:
-                    newFill = Fill()
-                    newFill.fill_type = patternFill.get('patternType')
-
-                    fgColor = patternFill.find('{%s}fgColor' % SHEET_MAIN_NS)
-                    if fgColor is not None:
-                        newFill.start_color.index = self._get_relevant_color(fgColor)
-
-                    bgColor = patternFill.find('{%s}bgColor' % SHEET_MAIN_NS)
-                    if bgColor is not None:
-                        newFill.end_color.index = self._get_relevant_color(bgColor)
-                    fill_list.append(newFill)
-        return fill_list
+            for fill_node in safe_iterator(fills, '{%s}fill' % SHEET_MAIN_NS):
+                yield self.parse_fill(fill_node)
 
 
-    def parse_borders(self, node=False):
+    def parse_fill(self, fill_node):
+        """Read individual fill"""
+        patternFill = fill_node.find('{%s}patternFill' % SHEET_MAIN_NS)
+        if patternFill is not None:
+            newFill = Fill()
+            newFill.fill_type = patternFill.get('patternType')
+            fgColor = patternFill.find('{%s}fgColor' % SHEET_MAIN_NS)
+            if fgColor is not None:
+                newFill.start_color.index = self._get_relevant_color(fgColor)
+            bgColor = patternFill.find('{%s}bgColor' % SHEET_MAIN_NS)
+            if bgColor is not None:
+                newFill.end_color.index = self._get_relevant_color(bgColor)
+            return newFill
+
+
+    def parse_borders(self):
         """Read in the boarders"""
-        border_list = []
-        if node:
-            borders = node
-        else:
-            borders = self.root.find('{%s}borders' % SHEET_MAIN_NS)
+        borders = self.root.find('{%s}borders' % SHEET_MAIN_NS)
         if borders is not None:
-            boarderNodes = safe_iterator(borders, '{%s}border' % SHEET_MAIN_NS)
-            for border in boarderNodes:
-                newBorder = Borders()
-                if border.get('diagonalup') == 1:
-                    newBorder.diagonal_direction = newBorder.DIAGONAL_UP
-                if border.get('diagonalDown') == 1:
-                    if newBorder.diagonal_direction == newBorder.DIAGONAL_UP:
-                        newBorder.diagonal_direction = newBorder.DIAGONAL_BOTH
-                    else:
-                        newBorder.diagonal_direction = newBorder.DIAGONAL_DOWN
+            for border_node in safe_iterator(borders, '{%s}border' % SHEET_MAIN_NS):
+                yield self.parse_border(border_node)
 
-                for side in ('left', 'right', 'top', 'bottom', 'diagonal'):
-                    node = border.find('{%s}%s' % (SHEET_MAIN_NS, side))
-                    if node is not None:
-                        borderSide = getattr(newBorder,side)
-                        if node.get('style') is not None:
-                            borderSide.border_style = node.get('style')
-                        color = node.find('{%s}color' % SHEET_MAIN_NS)
-                        if color is not None:
-                            borderSide.color.index = self._get_relevant_color(color)
-                            # Ignore 'auto'
-                            #if color.get('indexed') is not None and (
-                                #0 <= int(color.get('indexed')) < len(self.color_index)
-                                #):
-                                #borderSide.color.index = self.color_index[int(color.get('indexed'))]
-                            #elif color.get('theme') is not None:
-                                #if color.get('tint') is not None:
-                                    #borderSide.color.index = 'theme:%s:%s' % (color.get('theme'), color.get('tint'))
-                                #else:
-                                    #borderSide.color.index = 'theme:%s:' % color.get('theme')  # prefix color with theme
-                            #elif color.get('rgb'):
-                                #borderSide.color.index = color.get('rgb')
-                border_list.append(newBorder)
-        return border_list
+
+    def parse_border(self, border_node):
+        """Read individual border"""
+        newBorder = Borders()
+        if border_node.get('diagonalup') == 1:
+            newBorder.diagonal_direction = newBorder.DIAGONAL_UP
+        if border_node.get('diagonalDown') == 1:
+            if newBorder.diagonal_direction == newBorder.DIAGONAL_UP:
+                newBorder.diagonal_direction = newBorder.DIAGONAL_BOTH
+            else:
+                newBorder.diagonal_direction = newBorder.DIAGONAL_DOWN
+
+        for side in ('left', 'right', 'top', 'bottom', 'diagonal'):
+            node = border_node.find('{%s}%s' % (SHEET_MAIN_NS, side))
+            if node is not None:
+                borderSide = getattr(newBorder,side)
+                if node.get('style') is not None:
+                    borderSide.border_style = node.get('style')
+                color = node.find('{%s}color' % SHEET_MAIN_NS)
+                if color is not None:
+                    borderSide.color.index = self._get_relevant_color(color)
+                    # Ignore 'auto'
+        return newBorder
+
 
 
     def parse_cell_xfs(self):
